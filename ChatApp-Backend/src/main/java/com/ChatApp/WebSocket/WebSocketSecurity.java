d453bf3904cdbf42a6ffda25a57199535e667dd5
Sufyan-007 <sgazdhar@argusoft.com>
2023-11-21 14:26:40 +0530

    deleted:    ChatApp-Backend/src/main/java/com/ChatApp/WebSocket/WebSocketSecurity.java
    Changes not staged for commit:
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/Config/RestExceptionHandler.java
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/Config/UserAuthenticationProvider.java
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/Controllers/AuthController.java
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/Controllers/MessageController.java
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/Conversation/ConversationDto.java
    	modified:   ChatApp-Backend/src/main/java/com/ChatApp/WebSocket/WebSocketConfig.java
    	modified:   ChatApp-Frontend/src/app/app-routing.module.ts
    	modified:   ChatApp-Frontend/src/app/app.module.ts
    	modified:   ChatApp-Frontend/src/app/chats-scrollbar/chats.component.html
    	modified:   ChatApp-Frontend/src/app/chats-scrollbar/chats.component.ts
    	modified:   ChatApp-Frontend/src/app/conversation/conversation.component.html
    	modified:   ChatApp-Frontend/src/app/conversation/conversation.component.ts
    	modified:   ChatApp-Frontend/src/app/http-interceptor.interceptor.ts
    	modified:   ChatApp-Frontend/src/app/login/login.component.css
    	modified:   ChatApp-Frontend/src/app/login/login.component.html
    	modified:   ChatApp-Frontend/src/app/login/login.component.ts
    	modified:   ChatApp-Frontend/src/app/search-bar/search-bar.component.css
    	modified:   ChatApp-Frontend/src/app/search-bar/search-bar.component.ts
    	modified:   ChatApp-Frontend/src/app/service/conversation.service.ts
    	modified:   ChatApp-Frontend/src/app/service/user.service.ts
    	modified:   ChatApp-Frontend/src/app/service/web-socket.service.ts

--- ChatApp-Backend/src/main/java/com/ChatApp/WebSocket/WebSocketSecurity.java
+++
@@ -1,6 +0,0 @@
-//package com.ChatApp.WebSocket;
-//
-//import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-//
-//public class WebSocketSecurity extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-//}
