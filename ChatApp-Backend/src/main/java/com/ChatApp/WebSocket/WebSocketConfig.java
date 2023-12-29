88e1e864075a4274e586c3e4410af3e1f039f3f2
Sufyan-007 <139311956+Sufyan-007@users.noreply.github.com>
2023-12-29 10:50:57 +0530

    added origin port 3000

--- ChatApp-Backend/src/main/java/com/ChatApp/WebSocket/WebSocketConfig.java
+++ ChatApp-Backend/src/main/java/com/ChatApp/WebSocket/WebSocketConfig.java
@@ -13,9 +13,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200")
-                .withSockJS()
-        ;
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200", "http://localhost:3000")
+                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200", "http://localhost:3000");
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
